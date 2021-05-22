9669854173121d76f1907c63e031c24b4008d628
Betaniaoliveira <81036375+Betaniaoliveira@users.noreply.github.com>
2021-05-22 10:35:59 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+
+           const nome =prompt ("digite seu nome completo");
+           const ano =prompt ( "digite seu ano de nascimento");
+           const anodenascimento= 2021-ano;
+           alert(" seu nome é "  + nome + " e sua idade é " + anodenascimento + " anos " );
+
+            
+
+         
